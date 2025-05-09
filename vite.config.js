cb43f07c516fcbb181291eb575a9017f92066bca
Praneeth <praneethg807@gmail.com>
2025-05-09 08:23:29 +0530

    Fix: Clean up vite.config.js and remove tailwind import

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-
-
-
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './', // Important for relative paths in deployment
+  plugins: [react(),
+    tailwindcss(),
+
+  ],
 })
+
 
